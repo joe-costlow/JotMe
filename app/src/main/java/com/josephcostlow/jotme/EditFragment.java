1c07587c88ef9a74438565705e27f2619aec55e3
Joseph Costlow <jcostlow@hotmail.com>
2017-07-23 12:35:23 -0400

    Initial Commit

---
+++ app/src/main/java/com/josephcostlow/jotme/EditFragment.java
@@ -0,0 +1,134 @@
+package com.josephcostlow.jotme;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link EditFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link EditFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class EditFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+
+    TextView labelTitle, labelTags, labelMessage;
+    EditText editTitle, editTagOne, editTagTwo, editTagThree, editMessage;
+
+    public EditFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment EditFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static EditFragment newInstance(String param1, String param2) {
+        EditFragment fragment = new EditFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_edit, container, false);
+
+        labelTitle = (TextView) rootView.findViewById(R.id.edit_label_title);
+        labelTags = (TextView) rootView.findViewById(R.id.edit_label_tag);
+        labelMessage = (TextView) rootView.findViewById(R.id.edit_label_message);
+
+        editTitle = (EditText) rootView.findViewById(R.id.edit_edit_title);
+        editTagOne = (EditText) rootView.findViewById(R.id.edit_edit_tag_one);
+        editTagTwo = (EditText) rootView.findViewById(R.id.edit_edit_tag_two);
+        editTagThree = (EditText) rootView.findViewById(R.id.edit_edit_tag_three);
+        editMessage = (EditText) rootView.findViewById(R.id.edit_edit_message);
+
+        editTitle.setText(getResources().getText(R.string.sample_title));
+        editTagOne.setText(getResources().getText(R.string.sample_tag));
+        editTagTwo.setText(getResources().getText(R.string.sample_tag));
+        editTagThree.setText(getResources().getText(R.string.sample_tag));
+//        editMessage.setText(getResources().getText(R.string.sample_message));
+        editMessage.setText("This is a sample message");
+
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+//        if (context instanceof OnFragmentInteractionListener) {
+//            mListener = (OnFragmentInteractionListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
---
+++ app/src/main/java/com/josephcostlow/jotme/MainActivity.java
@@ -0,0 +1,43 @@
+package com.josephcostlow.jotme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    boolean mDualPane;
+
+    public ListFragment listFragment;
+    public DetailFragment detailFragment;
+    public EditFragment editFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDualPane = getResources().getBoolean(R.bool.dual_pane);
+
+        listFragment = new ListFragment();
+        detailFragment = new DetailFragment();
+        editFragment = new EditFragment();
+
+
+        if (mDualPane) {
+
+//            getSupportFragmentManager().beginTransaction()
+//                    .replace(R.id.frame_right, editFragment)
+//                    .replace(R.id.frame_right, detailFragment)
+//                    .replace(R.id.frame_left, listFragment)
+//                    .commit();
+
+        } else {
+
+//            getSupportFragmentManager().beginTransaction()
+//                    .replace(R.id.frame_full, detailFragment)
+//                    .replace(R.id.frame_full, editFragment)
+//                    .commit();
+
+        }
+    }
+}
